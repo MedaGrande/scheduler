adf181a6e3979fe6627148e7632dd77205821b35
Medhanie Alene <alenemedhanie@gmail.com>
2023-05-20 22:02:40 -0400

    Add unit test for DayListItem; add the function formatSpots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,17 +1,23 @@
 import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
+import { formatSpots } from "./formatSpots";
+
 
 export default function DayListItem(props) {
+  const { name, spots, setDay } = props;
+  const spotsMessage = formatSpots(spots);
+
   const dayClass = classNames({
     "day-list__item": true,
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots
   });
+  
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spotsMessage}</h3>
     </li>
   );
 }
---
+++ src/components/formatSpots.js
@@ -0,0 +1,7 @@
+export function formatSpots(spots) {
+  return spots === 0
+    ? "no spots remaining"
+    : spots === 1
+      ? "1 spot remaining"
+      : `${spots} spots remaining`;
+}
