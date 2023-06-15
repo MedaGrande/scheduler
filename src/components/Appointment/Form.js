d9163b1078b88a1b07efa799a2eecd999b1e4512
Medhanie Alene <alenemedhanie@gmail.com>
2023-06-14 20:54:57 -0400

    Add more tests to Form component & refactor Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,8 +9,9 @@ export default function Form(props) {
   const [error, setError] = useState("");
 
   function reset() {
-      setStudent("")
-      setInterviewer(null)
+    setStudent("")
+    setInterviewer(null)
+    setError("");
   }
 
   function cancel() {
@@ -28,7 +29,8 @@ export default function Form(props) {
     setError("Please select an interviewer");
     return;
   }  
-
+  
+  setError("");
   props.onSave(student, interviewer);
 }
 
@@ -57,7 +59,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => { validate(); }}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
